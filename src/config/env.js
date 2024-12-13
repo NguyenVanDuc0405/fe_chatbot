b7e6fe072a73c7fea415e555f9d0f5f5fb9ec74e
Admin <duchandsome0201@gmail.com>
2024-12-13 13:12:24 +0700

    update localhost

--- src/config/env.js
+++ src/config/env.js
@@ -1,5 +1,5 @@
-// const ENVIRONMENT_CONFIG = "http://127.0.0.1:5000";
-const ENVIRONMENT_CONFIG = "https://85cd-1-53-8-28.ngrok-free.app";
+const ENVIRONMENT_CONFIG = "http://127.0.0.1:5000";
+// const ENVIRONMENT_CONFIG = "https://85cd-1-53-8-28.ngrok-free.app";
 
 
 export default ENVIRONMENT_CONFIG;
